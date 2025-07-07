a129db714675224d5754b45d0223e640a329ee82
udardelano <gelamigia1@gmail.com>
2025-07-07 19:30:49 +0200

    Fixing raises

---
+++ 07072025_193049_raises_m3qwd.sh
@@ -0,0 +1,1 @@
+The Main Library at Indiana University sinks over an inch every year because when it was built, engineers failed to take into account the weight of all the books that would occupy the building
